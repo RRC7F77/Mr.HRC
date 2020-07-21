40bbaa0296e5bbec20c7926d6b0e4327103ae93e
Mr.RRC7F77 <68005854+RRC7F77@users.noreply.github.com>
2020-07-22 02:23:08 +0900

    Add files via upload

---
+++ Rrc7F77.php
@@ -0,0 +1,188 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo
+echo $yellow
+cat baca.txt
+sleep 10
+echo $green
+figlet "SPEED"
+echo $purple"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"Author : Zona Red"
+echo $cyan"Kontak : 081344834622"
+echo $red"SUBSCRIBE :" $yellow"The Zona Red"
+echo $purple"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"     SELAMAT DATANG DI ZONARED"
+echo
+echo $yellow"Silahkan Memilih Script :"
+echo
+echo $blue"1.) Stabilkan Jaringan v1"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $blue"2.) Stabilkan Jaringan v2"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"3.) Stabilkan Jaringan Saat Browsing"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"4.) Stabilkan Jaringan Saat nonton Youtube v1"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"5.) Stabilkan Jaringan Saat nonton Youtube v2"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"6.) Stabilkan Jaringan Saat Chatting Facebook v1"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $red"7.) Stabilkan Jaringan Saat Chatting Facebook v2"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $red"8.) Stabilkan Jaringan Saat Chatting Instagram"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $yellow"9.) Coming Soon...."
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"0.) KELUAR AJALAH"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green
+figlet "BONUS"
+echo " PILIH BONUS...(Ketik Buka)"
+echo $cyan"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green
+read -p "Silahkan Pilih No Berapa : " Sodara
+
+if [ $sodara = 1 ] || [ $sodara = 1 ]
+then
+clear
+toilet -f big -F gay Yadi
+echo $green"Stabilkan Jaringan v1 by Mr. Rrc7F77"
+sleep 1
+echo
+ping -s1000 1.1.1.1
+fi
+
+if [ $sodara = 2 ] || [ $sodara = 2 ]
+then
+clear
+toilet -f big -F gay Mr. Rrc7F77
+echo $yellow"Stabilkan Jaringan v2 by Mr. Rrc7F77"
+sleep 1
+echo
+ping -s1000 8.8.8.8
+fi
+
+if [ $sodara = 3 ] || [ $sodara = 3 ]
+then
+clear
+echo $green
+figlet "By Mr. Rrc7F77"
+echo $white"Insya Allah Browsing Lancar"
+sleep 1
+echo
+ping -s1000 216.239.38.120
+fi
+
+if [ $sodara = 4 ] || [ $sodara = 4 ]
+then
+clear
+echo $yellow
+figlet "Y T v 1"
+echo "Stabilkan jaringan saat nonton Youtube v1 by Mr. Rrc7F77"
+sleep 2
+echo
+ping -s1000 74.125.24.91
+fi
+
+if [ $sodara = 5 ] || [ $sodara = 5 ]
+then
+clear
+echo $yellow
+figlet "Y T v 2"
+echo "Stabilkan jaringan saat nonton Youtube v2 by Mr. Rrc7F77"
+sleep 2
+echo
+ping -s1000 172.217.194.113
+fi
+
+if [ $sodara = 6 ] || [ $sodara = 6 ]
+then
+clear
+echo $green
+figlet "F B v 1"
+echo "Stabilkan jaringan saat Chatting Facebook v1 by Mr. Rrc7F77"
+sleep 2
+echo
+ping -s1000 157.240.7.35
+fi
+
+if [ $sodara = 7 ] || [ $sodara = 7 ]
+then
+clear
+echo $blue
+figlet "F B v 2"
+echo "Stabilkan jaringan saat Chatting Facebook v2 by Mr. Rrc7F77"
+sleep 2
+echo
+ping -s1000 157.240.25.35
+fi
+
+if [ $sodara = 8 ] || [ $sodara = 8 ]
+then
+clear
+echo $purple
+figlet "I G"
+echo "Stabilkan jaringan saat Chatting Instagram by Mr. Rrc7F77"
+sleep 2
+echo
+ping -s1000 157.240.7.174
+fi
+
+if [ $sodara = 9 ] || [ $sodara = 9 ]
+then
+clear
+echo $white
+figlet "Coming"
+figlet "Soon"
+sleep 2
+echo $green"Tunggu script Ter update berikutnya"
+sleep 3
+echo $yellow"SEMOGA SCRIPT INI BERMANFAAT"
+sleep 2
+echo $white"Sampai Jumpa Lagi Di Script Berikutnya....:)"
+sleep 2
+exit
+fi
+
+if [ $sodara = 0 ] || [ $sodara = 0 ]
+then
+clear
+echo $yellow
+figlet "Mr. Rrc7F77."
+echo $green"SEMOGA HARIMU MENYENANGKAN...:)"
+sleep 2
+echo $white"     TO BE CONTINUED..."
+echo
+sleep 1
+exit
+fi
+
+if [ $sodara = Buka ] || [ $sodara = Buka ]
+then
+clear
+echo $yellow
+figlet "Dark-Fb"
+sleep 2
+echo $green"Apakah Kamu Yakin Ingin Menginstall Dark-Fb v1.6 By Mr. Rrc7F77 ?"
+sleep 3
+echo $yellow"Kalau tidak tekan ctrl + c"
+sleep 2
+echo $white"Tapi Kalau Iya, Tunggu Sampai 15 detik"
+sleep 15
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/YadiFernando/DARKFB
+mv DARKFB $HOME
+cd $HOME/DARKFB
+python2 DARKFB.py
+fi
