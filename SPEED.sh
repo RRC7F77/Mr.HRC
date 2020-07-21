95e3d3d9f0fd9e4ec251a2adbf3e62bcc93bf99b
Mr.RRC7F77 <68005854+RRC7F77@users.noreply.github.com>
2020-07-22 03:24:10 +0900

    Speed22

--- SPEED.sh
+++ SPEED.sh
@@ -8,17 +8,17 @@ white='\033[37;1m'
 yellow='\033[33;1m'
 sleep 1
 echo
-echo $red
+echo $yellow
 cat baca.txt
 sleep 10
 echo $green
-figlet "SPEED"
+figlet "Speed22"
 echo $purple"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
-echo $white"Author : Yadi Fernando"
-echo $cyan"Kontak : 085706249638"
-echo $red"SUBSCRIBE :" $yellow"Official Yadi Fernando"
+echo $white"Author : Mr. RRC7F77"
+echo $cyan"Kontak : 081344834622"
+echo $red"SUBSCRIBE :" $yellow"BETAZONA"
 echo $purple"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
-echo $green"     SELAMAT DATANG BRO"
+echo $green"     SELAMAT DATANG SOBAT"
 echo
 echo $yellow"Silahkan Pilih Script Kesukaanmu :"
 echo
@@ -40,20 +40,20 @@ echo $purple"8.) Stabilkan Jaringan Saat Chattingan Instagram"
 echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
 echo $white"9.) Coming Soon...."
 echo $yellow"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
-echo $red"0.) KELUAR AJALAH"
+echo $red"0.) KELUAR"
 echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
 echo $green
 figlet "BONUS"
 echo " BUKA BONUS...(Ketik Buka)"
 echo $cyan"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
 echo $green
-read -p "Silahkan Pilih No Berapa : " bro
+read -p "Silahkan Pilih No Berapa : " sobat
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-toilet -f big -F gay Yadi
-echo $green"Stabilkan Jaringan v1 by Yadi"
+toilet -f big -F gay Mr. RRC7F77
+echo $green"Stabilkan Jaringan v1 by Mr. RRC7F77"
 sleep 1
 echo
 ping -s1000 1.1.1.1
@@ -62,8 +62,8 @@ fi
 if [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
-toilet -f big -F gay Yadi
-echo $yellow"Stabilkan Jaringan v2 by Yadi"
+toilet -f big -F gay Mr. RRC7F77
+echo $yellow"Stabilkan Jaringan v2 by Mr. RRC7F77"
 sleep 1
 echo
 ping -s1000 8.8.8.8
@@ -72,9 +72,9 @@ fi
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
 clear
-echo $green
-figlet "By Yadi"
-echo $white"Insya Allah Browsing Lancar"
+echo $blue
+figlet "By Mr. RRC7F77"
+echo $yellow"Semoga Ale Browsing Lancar"
 sleep 1
 echo
 ping -s1000 216.239.38.120
@@ -85,7 +85,7 @@ then
 clear
 echo $red
 figlet "Y T v 1"
-echo "Stabilkan jaringan saat nonton Youtube v1 by Yadi"
+echo "Stabilkan jaringan saat nonton Youtube v1 by Mr. RRC7F77"
 sleep 2
 echo
 ping -s1000 74.125.24.91
@@ -96,7 +96,7 @@ then
 clear
 echo $red
 figlet "Y T v 2"
-echo "Stabilkan jaringan saat nonton Youtube v2 by Yadi"
+echo "Stabilkan jaringan saat nonton Youtube v2 by Mr. RRC7F77"
 sleep 2
 echo
 ping -s1000 172.217.194.113
@@ -107,7 +107,7 @@ then
 clear
 echo $blue
 figlet "F B v 1"
-echo "Stabilkan jaringan saat Chattingan Facebook v1 by Yadi"
+echo "Stabilkan jaringan saat Chattingan Facebook v1 by Mr. RRC7F77"
 sleep 2
 echo
 ping -s1000 157.240.7.35
@@ -118,7 +118,7 @@ then
 clear
 echo $blue
 figlet "F B v 2"
-echo "Stabilkan jaringan saat Chattingan Facebook v2 by Yadi"
+echo "Stabilkan jaringan saat Chattingan Facebook v2 by Mr. RRC7F77"
 sleep 2
 echo
 ping -s1000 157.240.25.35
@@ -129,7 +129,7 @@ then
 clear
 echo $purple
 figlet "I G"
-echo "Stabilkan jaringan saat Chattingan Instagram by Yadi"
+echo "Stabilkan jaringan saat Chattingan Instagram by Mr. RRC7F77"
 sleep 2
 echo
 ping -s1000 157.240.7.174
@@ -142,11 +142,11 @@ echo $white
 figlet "Coming"
 figlet "Soon"
 sleep 2
-echo $green"Tunggu script Ter update berikutnya"
+echo $white"Tunggu script Ter update berikutnya"
 sleep 3
-echo $yellow"SEMOGA SCRIPT INI BERMANFAAT"
+echo $green"SEMOGA SCRIPT INI BERMANFAAT"
 sleep 2
-echo $white"Sampai Jumpa Lagi Di Script Berikutnya....:)"
+echo $yellow"Sampai Jumpa Lagi Di Script Berikutnya....:)"
 sleep 2
 exit
 fi
@@ -155,8 +155,8 @@ if [ $bro = 0 ] || [ $bro = 0 ]
 then
 clear
 echo $yellow
-figlet "Yadi  F."
-echo $green"SEMOGA HARIMU MENYENANGKAN...:)"
+figlet "Mr. RRC7F77  F."
+echo $green"SEMOGA ALE BERUNTUNG SOBAT...:)"
 sleep 2
 echo $white"     TO BE CONTINUED..."
 echo
@@ -170,18 +170,18 @@ clear
 echo $yellow
 figlet "Dark-Fb"
 sleep 2
-echo $green"Apakah Kamu Yakin Ingin Menginstall Dark-Fb v1.6 By Yadi ?"
+echo $green"Apakah Kamu Yakin Ingin Menginstall Dark-Fb v1.6 By Mr. RRC7F77 ?"
 sleep 3
 echo $yellow"Kalau tidak tekan ctrl + c"
 sleep 2
-echo $white"Tapi Kalau Iya, Tunggu Sampai 15 detik"
-sleep 15
+echo $white"Tapi Kalau Iya, Tunggu Sampai 10 detik"
+sleep 10
 pkg update && pkg upgrade
 pkg install git
 pkg install python2
 pip2 install mechanize
 pip2 install requests
-git clone https://github.com/YadiFernando/DARKFB
+git clone https://github.com/RRC7F77/DARKFB
 mv DARKFB $HOME
 cd $HOME/DARKFB
 python2 DARKFB.py
